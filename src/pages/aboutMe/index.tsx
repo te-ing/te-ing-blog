532bf7da655753919ba5b100fd054bab2a0dc01a
te-ing <hi2177@naver.com>
2024-07-14 16:57:01 +0900

    feat: create resume with temp image

--- src/pages/aboutMe/index.tsx
+++ src/pages/aboutMe/index.tsx
@@ -1,12 +1,14 @@
 import Layout from "@theme/Layout";
+import styles from './styles.module.css';
 
 export default function Home(): JSX.Element {
   return (
     <Layout
       title={`김태중 블로그`}
       description="Description will go into a meta tag in <head />">
-      <main>
-        TEST
+      <main className={styles.wrapper} >
+        <img alt="resume first page" className={styles.resume1} />
+        <img alt="resume second page" className={styles.resume2} />
       </main>
     </Layout>
   );
