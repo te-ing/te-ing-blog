3445a7a637838969dfe8185234bc22b261e99605
te-ing <hi2177@naver.com>
2024-07-21 22:16:42 +0900

    feat: Add About Me item: Work

--- src/pages/aboutMe/Work.tsx
+++ src/pages/aboutMe/Work.tsx
@@ -1,6 +1,7 @@
 import { getYearMonthDifference } from '@site/src/util/dateUtil';
 import styles from './work.module.css';
 import React from 'react';
+import WorkCareers from './WorkCareers';
 
 const Work = () => {
   return (
@@ -17,13 +18,14 @@ const Company = () => {
     <div className={styles.companyContainer}>
       <h2>Work Experience</h2>
       <div className={styles.company}>
-        <div>
+        <div className={styles.companyHeader}>
           <h3>SNPLAB</h3>
           {`2022.08 ~ 재직중 (약 ${getYearMonthDifference(new Date('2022-08-01'))})`}
         </div>
         <span className={styles.subTitle}>
           개인정보 마이데이터 서비스 스타트업
         </span>
+        <WorkCareers />
       </div>
     </div>
   );
---
+++ src/pages/aboutMe/WorkCareers.tsx
@@ -0,0 +1,45 @@
+import { CAREER_DATA } from './resumeConstants';
+import styles from './workCareers.module.css';
+import React from 'react';
+
+export default function WorkCareers() {
+  const careers = CAREER_DATA;
+  return (
+    <div className={styles.wrapper}>
+      {careers.map(career => {
+        return (
+          <div className={styles.container}>
+            <div className={styles.leftContainer}>
+              <span>{career.period}</span>
+              <hr />
+              <span className={styles.skillName}>사용기술</span>
+              <div className={styles.skill}>
+                {career.skill.map(v => (
+                  <span id={v}>‣ {v}</span>
+                ))}
+              </div>
+            </div>
+
+            <div className={styles.rightContainer}>
+              <h3>{career.title}</h3>
+              {career.content.map(({ title, body }) => {
+                return (
+                  <ul className={styles.careerTitle}>
+                    <li>{title}</li>
+                    {body?.map(item => {
+                      return (
+                        <ul className={styles.careerItem}>
+                          <li>{item}</li>
+                        </ul>
+                      );
+                    })}
+                  </ul>
+                );
+              })}
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
