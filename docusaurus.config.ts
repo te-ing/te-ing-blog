453f276f1a67f11caf27291d56afb7591afb1ca8
te-ing <hi2177@naver.com>
2024-08-03 16:57:39 +0900

    feat: scss plugin 추가

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -46,7 +46,7 @@ const config: Config = {
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: './src/css/custom.scss',
         },
       } satisfies Preset.Options,
     ],
@@ -114,6 +114,8 @@ const config: Config = {
       darkTheme: prismThemes.dracula,
     },
   } satisfies Preset.ThemeConfig,
+
+  plugins: ['docusaurus-plugin-sass'],
 };
 
 export default config;
