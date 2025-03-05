65a558b9826d905bada0982546c2a08d8f0e138e
te-ing <hi2177@naver.com>
2025-03-05 13:31:05 +0900

    feat: Giscus를 사용한 댓글 기능 구현

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,6 +11,7 @@ const config: Config = {
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/te-ing-blog/',
+  trailingSlash: false,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
